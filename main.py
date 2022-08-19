c2395da2f57d11bcff4bb76a4180d3a3baa3ca25
Vladyslav Bohachev <94163529+vladyslavbohachev@users.noreply.github.com>
2022-08-19 13:33:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,28 @@ import time
 import shutil
 
 dirpath = 'X:/'
-creationdatefolder = 'X:/'
 
-directoryList = [creationdatefolder]
-for items in directoryList:
-    os.makedirs(items, exist_ok=True)
+def sort():
+    directoryList = [dirpath]
+    for items in directoryList:
+        os.makedirs(items, exist_ok=True)
 
-for root, dirs, files in os.walk(dirpath):
+    for root, dirs, files in os.walk(dirpath):
         if dirpath is None:
             exit(0)
         else:
             for filename in files:
                 if filename.endswith('.xml'):
-                    modificationTime = time.strftime('%d.%m.%Y', time.localtime(os.path.getmtime(os.path.join(dirpath, filename))))
+                    modificationTime = time.strftime('%d.%m.%Y',
+                                                     time.localtime(os.path.getmtime(os.path.join(dirpath, filename))))
                     dirlist = [modificationTime]
                     for items in dirlist:
-                        os.makedirs(dirpath+items, exist_ok=True)
-                    shutil.move(os.path.join(dirpath, filename), os.path.join(creationdatefolder + str(modificationTime), filename))
+                        os.makedirs(dirpath + items, exist_ok=True)
+                    shutil.move(os.path.join(dirpath, filename),
+                                os.path.join(dirpath + str(modificationTime), filename))
+        return 0
+def main():
+    sort()
+
+if __name__ == '__main__':
+    main()
